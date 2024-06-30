1a87a00360d2e17b2bbdb5f3769b7c91da530d30
nz-cloud-udemy <162379447+nz-cloud-udemy@users.noreply.github.com>
2024-06-30 20:13:31 +1200

    Update init-ingress.sh

--- practice-questions/11/init-ingress.sh
+++ practice-questions/11/init-ingress.sh
@@ -1,4 +1,4 @@
 helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace
 
 NODE_IP=$(kubectl get nodes controlplane -o jsonpath='{.status.addresses[?(@.type=="InternalIP")].address}')
-echo $NODE_IP ckad-ingress.info >> /etc/hosts
+echo $NODE_IP path-ingress.info >> /etc/hosts
